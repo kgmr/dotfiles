535ac85e8217cbf8fcd5dc035f029b916a589e34
Kássio Guimarães <kassioguimaraes@protonmail.com>
2017-03-01 01:06:48 -0300

    i3bar

---
+++ home/.i3bar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+i3status --config ~/.i3status.conf | while :
+do
+        read line
+	if [ $(pidof mpd) ];then
+		if [ "$(mpc status | grep pause | cut -d ' ' -f1)" = "[paused]" ]; then 
+			symbol=""
+		else
+			symbol=""
+		fi
+        	playing=$(mpc current)
+        	echo "$symbol  $playing | $line" || exit 1
+	else
+		echo $line || exit 1
+	fi
+done
